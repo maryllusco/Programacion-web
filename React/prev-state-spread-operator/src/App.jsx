9adf741cb41be7323e437e37491abbe77860a9c0
mary llusco <maryluzllusco03@gmail.com>
2025-08-27 17:32:21 -0300

    termine la tarea, consegui 7 puntitos

--- React/prev-state-spread-operator/src/App.jsx
+++ React/prev-state-spread-operator/src/App.jsx
@@ -1,36 +1,44 @@
-
 import { useState } from 'react'
 import './App.css'
 
 function App() {
-const [num,setNum] = useState([1,2,3,4])
-const [persona, setPersona] = useState ({nombre:"mary", edad: 17})
-//Spread Operator//
-function myFuction (){
-  const ultimo = num[num.length - 1]
-  setNum([...num, ultimo + 1]);
+  const [num, setNum] = useState([1, 2, 3, 4])
+  const [persona, setPersona] = useState({ nombre: "mary", edad: 17 })
 
-}
-function persona(){
-   const nPersona = ({
-      ...persona,           // copiamos las propiedades que ya tenía
-      nacionalidad: "Argentina"   // le agregamos una nueva propiedad
-    })
-}
-//tarea, hacer que el setNum agregue numeros consecutivos. -> 3 puntos
-//agregar una propiedad al objeto persona usando spread operator -> 4 puntos 
+  // Agregar números consecutivos 3 puntitos
+  function myFuction() {
+    const ultimo = num[num.length - 1]
+    setNum([...num, ultimo + 1])
+  }
+
+  // Agregar propiedad nacionalidad 4 puntitos
+  function personaFun() {
+    const nPersona = {
+      ...persona,
+      nacionalidad: "Argentina"
+    }
+    setPersona(nPersona)
+  }
 
   return (
     <>
-    <div>
-      {num.map((item, index) => (
-        <p>{item}</p>
-      ))}
-      <button onClick={myFuction}>Agregar numero</button>
-      <button onClick={persona}> Agregue propiedad</button>
-    </div>
+      <div>
+        <h3>Lista de números</h3>
+        {num.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+        <button onClick={myFuction}>Agregar número</button>
+      </div>
+
+      <div>
+        <h3>Datos de persona</h3>
+        <p>Nombre: {persona.nombre}</p>
+        <p>Edad: {persona.edad}</p>
+        <p>Nacionalidad: {persona.nacionalidad}</p>
+        <button onClick={personaFun}>Agregar propiedad</button>
+      </div>
     </>
   )
 }
-    
+
 export default App
